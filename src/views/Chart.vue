e666788a6c293f4c22bf4bfd870fc2016d898edd
amber0216 <d10632106@gapps.fg.tp.edu.tw>
2022-05-11 21:29:22 +0800

    做出一個圖表

--- src/views/Chart.vue
+++ src/views/Chart.vue
@@ -1,24 +1,21 @@
 <template>
-    <p>this is Chart</p>
+  <vc-donut
+    :sections="[{ value: 25, text:'a' }, { value: 25 }, { value: 25 }, { value: 25 }]"
+    :thickness="30">
+  </vc-donut>
 </template>
-
 <script>
-export default {
-    name: "Chart",
+  export default {
     data() {
-        return {
-
-        }
-    },
-    mounted() {
-
-    },
-    methods: {
-
-    },
-    computed: {
-
+      return {
+        sections: [
+          { value: 25, color: '#31c0d6'}, 
+          { value: 25, color: '#e66e53'},
+          { value: 25, color: '#b184d1'},
+          { value: 25, color: '#f5b042'},
+          ],
+         
+      };
     }
-}
-
+  };
 </script>
