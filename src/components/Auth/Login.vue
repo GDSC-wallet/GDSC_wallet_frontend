bdda107a4ec950551622d1910fe4cceb80dff05e
andyjjrt <andyjjrt@gmail.com>
2022-05-12 13:56:52 +0800

    andyjjr

---
+++ src/components/Auth/Login.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <v-btn elevation="2" href="/oauth/google/login">Google Login</v-btn>
+    </div>
+</template>
+
+<script>
+</script>
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,17 @@
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  devServer: {
+    proxy: {
+      '^/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true
+      },
+      '^/oauth': {
+        target: 'http://localhost:3000',
+        changeOrigin: true
+      },
+    }
+  }
 }
